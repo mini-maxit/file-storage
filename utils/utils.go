b75c63aabe1e8cce87fc4a037e5bcc6ccf2693ce
Mateusz Polis <polismateusz@gmail.com>
2024-11-01 18:23:48 +0100

    Task/accept archives (#2)
    * Add InitializeRootDirectory.
    
    * Add CreateTaskDirectory.
    
    * Add CreateUserSubmission.
    
    * Add StoreUserOutputs. Create a Readme.md file.
    
    * Add GetTaskFiles.
    
    * Add GetUserSubmission.
    
    * Add GetInputOutput.
    
    * Add DeleteTask.
    
    * Add GetUserSolutionPackage.
    
    * Move less important functions from task_service.go to task_utils.go. Save tasks in the subdirectory /tasks of root directory.
    
    * Move less important functions from task_service.go to task_utils.go. Save tasks in the subdirectory /tasks of root directory.
    
    * Use {number}.in/out instead of {number}.in/out.txt. Don't rename files upon saving.
    
    * Fix: restore directory only when there had been one before.
    
    * Fix all the code warnings.
    
    * Fix all the code warnings.
    
    * Fix Readme.md.
    
    * Fix Readme.md.
    
    * Add Usage to Readme.md.
    
    * Add better error handling.
    
    * Fix PR issues.
    
    * Fix tests.
    
    * Accept Archives instead of lists of files.
    
    * Update Readme.md.

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,17 @@
 package utils
 
 import (
+	"archive/tar"
+	"archive/zip"
+	"compress/gzip"
+	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 // FileSize returns size of file
@@ -27,4 +35,118 @@ func RemoveDirectory(path string) {
 	if err := os.RemoveAll(path); err != nil {
 		log.Printf("Error removing directory: %v", err)
 	}
+}
+
+// DecompressArchive decompresses archive (either .zip or .tar.gzip) to the given newPath
+func DecompressArchive(archivePath string, newPath string) error {
+	if strings.HasSuffix(archivePath, ".gz") {
+		err := DecompressGzip(archivePath, newPath)
+		if err != nil {
+			return fmt.Errorf("failed to uncompress directory (gzip): %v", err)
+		}
+	} else if strings.HasSuffix(archivePath, ".zip") {
+		err := DecompressZip(archivePath, newPath)
+		if err != nil {
+			return fmt.Errorf("failed to uncompress directory (zip): %v", err)
+		}
+	} else {
+		return fmt.Errorf("unsupported archive type: %s", archivePath)
+	}
+
+	return nil
+}
+
+// DecompressGzip decompresses a Gzip archive from archivePath to a new directory in the newPath
+func DecompressGzip(archivePath string, newPath string) error {
+	file, err := os.Open(archivePath)
+	if err != nil {
+		return err
+	}
+	defer CloseIO(file)
+
+	uncompressedStream, err := gzip.NewReader(file)
+	if err != nil {
+		return err
+	}
+	defer CloseIO(uncompressedStream)
+
+	tarReader := tar.NewReader(uncompressedStream)
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			dirPath := path.Join(newPath, header.Name)
+			if err := os.MkdirAll(dirPath, 0755); err != nil {
+				return err
+			}
+
+		case tar.TypeReg:
+			filePath := path.Join(newPath, header.Name)
+			if err := os.MkdirAll(path.Dir(filePath), 0755); err != nil {
+				return err
+			}
+
+			outFile, err := os.Create(filePath)
+			if err != nil {
+				return err
+			}
+			defer CloseIO(outFile)
+
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				return err
+			}
+
+		default:
+			return errors.New("unsupported file type")
+		}
+	}
+	return nil
+}
+
+// DecompressZip decompresses a Gzip archive from archivePath to a new directory in the newPath
+func DecompressZip(archivePath string, newPath string) error {
+	r, err := zip.OpenReader(archivePath)
+	if err != nil {
+		return err
+	}
+	defer CloseIO(r)
+
+	for _, f := range r.File {
+		filePath := filepath.Join(newPath, f.Name)
+
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(filePath, 0755); err != nil {
+				return err
+			}
+		} else {
+			if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+				return err
+			}
+
+			inFile, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer CloseIO(inFile)
+
+			outFile, err := os.Create(filePath)
+			if err != nil {
+				return err
+			}
+			defer CloseIO(outFile)
+
+			if _, err := io.Copy(outFile, inFile); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
